fba8da9945d0ddf02a0a194bdbe8adf47181211c
Anurag Negi <123005508+Anurag-Negi28@users.noreply.github.com>
2024-01-06 22:15:08 +0530

    Uploading Frontend Part

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -7,14 +7,14 @@ function Navbar() {
  return (
    <nav className='navbar'>
      <div className='left'>
-       <img src="Rashtriya-Ispat-Nigam.jpg" className='navimg' alt="Left" />
+       <img src="./Rashtriya-Ispat-Nigam.jpg" className='navimg' alt="Left" />
        <a href="/" className='nav-head'>{t('Rashtriya Ispat Nigam')}</a>
      </div>
      <div className='right'>
        <a href="/login">
          <button className='loginBtn'>{t('Login')}</button>
        </a>
-       <img src="SIH.jpeg" className='navimg rightImg' alt="Right" />
+       <img src="./SIH.jpeg" className='navimg rightImg' alt="Right" />
      </div>
    </nav>
  );
